2f24cbbc2c8604dabc7621ce2a16e48ce5787adc
franc <franc@PATAGUA>
2023-05-24 20:31:24 -0300

    Se modifica la busqueda por DNI y se le asigna in ID a cada paseador.

--- tp2_Grupo38/src/main/java/ar/edu/unju/fi/model/Servicio.java
+++ tp2_Grupo38/src/main/java/ar/edu/unju/fi/model/Servicio.java
@@ -1,24 +1,39 @@
 package ar.edu.unju.fi.model;
 
 public class Servicio {
+	private String id;
 	private String dni;
 	private String paseador;
 	private String edad;
 	private String domicilio;
 	private String telefono;
 	private String honorarios;
+	private static int nextId = 1;
+	
 	public Servicio() {
 		
 	}
 
 	public Servicio(String dni,String nombre, String edad, String domicilio, String telefono, String honorarios) {
 		super();
+		this.id = "PAS-" + nextId++;
 		this.dni = dni;
 		this.paseador = nombre;
 		this.edad = edad;
 		this.domicilio = domicilio;
 		this.telefono = telefono;
 		this.honorarios = honorarios;
+	}
+	public static int getNextId() {//método getNextId() para obtener el próximo ID incremental.
+	    return nextId++;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public String getDni() {
